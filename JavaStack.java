15aea59f252f5654e6520931ab0e9b78c774b8d1
Akeem <akeem.mighty@gmail.com>
2022-10-17 23:23:35 -0400

    Create Stack Books Object + Add Book to Books Stack

--- JavaStack.java
+++ JavaStack.java
@@ -1,5 +1,14 @@
+import java.util.*;
+
 public class JavaStack {
     public static void main(string[] args) {
-        
+        Stack<String> books = new Stack<String>();
+        books.push("To Kill a Mockingbird");
+        books.push("The Hunger Games");
+        books.push("The Da Vinci Code");
+        books.push("Water for Elephants");
+        books.push("Lord of the Flies");
+
+
     }
 }
